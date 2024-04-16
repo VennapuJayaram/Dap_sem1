6052dc329ab140eb1ba00781fa5455fcde636b0d
Jayaram Vennapu <x22219978@student.ncirl.ie>
2024-04-16 16:18:13 +0530

    updated ingest

--- mongo_ingest.py
+++ mongo_ingest.py
@@ -7,12 +7,11 @@ db = client['dapdatabase']  # Replace 'your_database' with your database name
 collection = db['title']  # Replace 'your_collection' with your collection name
  
 # Read CSV file into a pandas DataFrame
-df = pd.read_csv(r)  # Replace 'your_file.csv' with the path to your CSV file
+df = pd.read_csv(r"C:\Users\venna\Desktop\DAP project\Electric_Vehicle_Title_and_Registration_Activity.csv")  # Replace 'your_file.csv' with the path to your CSV file
  
 # Convert DataFrame to a list of dictionaries (one dictionary per row)
 data = df.to_dict(orient='records')
  
-# Insert data into MongoDB
 collection.insert_many(data)
  
 print("Data inserted successfully.")
